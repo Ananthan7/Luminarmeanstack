be6027428f01d3f862d0a89b50908a1567d4ce45
Anantha krishnan <ananthankrishnan77@gmail.com>
2021-03-30 20:34:34 +0530

    javascript programs

---
+++ javascriptprograms/languagefundamentals/languagefun.js
@@ -0,0 +1,8 @@
+    // var age=20
+    // var name="Ananthan"
+    // console.log("hai i am"+ name + " age is " + age)
+    
+    var company="Luminar"
+    var location="kakkanad"
+
+    console.log(`our company ${company} is located in ${location}`);
---
+++ javascriptprograms/languagefundamentals/swapping.js
@@ -0,0 +1,13 @@
+var num1=10
+var num2=20
+
+var temp=num1
+var num1=num2
+var num2=temp
+
+// swapping
+// var num1=num1+num2
+// var num2=num1-num2
+// var num1=num1-num2
+
+console.log(`num1 is ${num1} \n num2 is ${num2}`);
